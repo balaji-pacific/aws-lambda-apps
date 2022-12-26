bdc8256b3d8ae4ac05fb93956d8e75622692cf36
Balaji Soundarrajan <balaji.pacific@gmail.com>
2022-12-25 21:54:08 -0500

    Adding S3 changes

--- src/main/java/com/lambda/io/InputObject.java
+++ src/main/java/com/lambda/io/InputObject.java
@@ -29,7 +29,12 @@ public class InputObject implements Serializable{
 	public void setAge(int age) {
 		this.age = age;
 	}
-	
-	
 
+	@Override
+	public String toString() {
+		return "InputObject{" +
+				"name='" + name + '\'' +
+				", age=" + age +
+				'}';
+	}
 }
--- src/main/java/com/lambda/io/WelcomeHelloWorld.java
+++ src/main/java/com/lambda/io/WelcomeHelloWorld.java
@@ -12,11 +12,17 @@ public class WelcomeHelloWorld implements RequestHandler<InputObject, Object>
 
 	@Override
 	public Object handleRequest(InputObject input, Context context) {
-		System.out.println("Welcome to lambda function Hellow World");
-		if(input.getAge() > 18) {
-			System.out.println(input.getName() + " is eligible for casting vote");
-		}else {
-			System.out.println(input.getName() + " is not eligible for casting vote");
+		System.out.println("Welcome to lambda function Hello World");
+		if(null != input){
+			System.out.println(input);
+			System.out.println("Value of Name and Age is : " + input.getName() +  " " + input.getAge());
+			if(input.getAge() > 18) {
+				System.out.println(input.getName() + " is eligible for casting vote");
+			}else {
+				System.out.println(input.getName() + " is not eligible for casting vote");
+			}
+		}else{
+			System.out.println("Input object is empty");
 		}
 		return "Hello World".toString();
 	}
